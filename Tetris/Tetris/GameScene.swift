2f5235f7ae471099942ab3164a5d5310be64c664
Mikel <mikel.harnisch@gmail.com>
2018-03-06 14:12:52 +0100

    06.03 Doc + Code update

--- Tetris/Tetris/GameScene.swift
+++ Tetris/Tetris/GameScene.swift
@@ -7,10 +7,54 @@
 //
 
 import SpriteKit
+let TickLengthLevelOne = TimeInterval(600)      //Define the speed that our shape will travel
 
 class GameScene: SKScene {
 
+
+    var tick:(() -> ())?                        //Won't take or return any parameter
+    var tickLengthMillis = TickLengthLevelOne
+    var lastTick:Date?                          //Will track the last time a tick was experienced
+    
+    required init(coder aDecoder: NSCoder){
+        fatalError("NSCoder not supported")
+    }
+    
+    override init(size: CGSize){
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0, y: 1.0)
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        
+        background.position = CGPoint(x: 0, y: 0)               //The game will be built from the top-left
+        background.anchorPoint = CGPoint(x:0 , y: 1.0)           //The anchor point (top left)
+        addChild(background)
+    }
+    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        guard let lastTick = lastTick else {    //Guard check if lastTick = LastTick, if fails execute the else block
+            return
+        }
+        
+        let timePassed = lastTick.timeIntervalSinceNow * -1000.0    //Calculate the time passed if lastTick is present
+        
+        if timePassed > tickLengthMillis{      //Report a tick if the time passed is greater than tickLenghtMillis
+            self.lastTick = Date()
+            tick?()
+        }
+    }
+    
+    //External class to let external class stop and start the ticking proccess
+    func startTicking(){
+        lastTick = Date()
+    }
+    
+    func stopTicking(){
+        lastTick = nil
     }
 }
+
+
+
--- Tetris/Tetris/GameViewController.swift
+++ Tetris/Tetris/GameViewController.swift
@@ -11,9 +11,25 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!                       //Type : GameScene, Name : scene
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        }
+        //Configure the view
+        let skView = view as! SKView            //Force view to be seen as an SKView item and not UIView
+        skView.isMultipleTouchEnabled = false
+        
+        //Create and configure the scene
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        
+        //Present scene
+        skView.presentScene(scene)
+    }
     
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
 }
+
