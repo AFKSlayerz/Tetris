f2861bcc2e711561540b3aa6293740bb2b97bf7b
Mikel <mikel.harnisch@gmail.com>
2018-03-09 15:07:03 +0100

    Code update

--- Tetris/Tetris/GameViewController.swift
+++ Tetris/Tetris/GameViewController.swift
@@ -11,13 +11,22 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
+    var clickCount = 0
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBAction func buttonClick(_ sender: UIButton) {
+        timeLabel.text="You clicked the button \(clickCount) times"
+    }
+
     var scene: GameScene!                       //Type : GameScene, Name : scene
+    var GameInt = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Configure the view
-        let skView = view as! SKView            //Force view to be seen as an SKView item and not UIView
+        // Configure the view.
+        let skView = view as! SKView
         skView.isMultipleTouchEnabled = false
         
         //Create and configure the scene
@@ -26,6 +35,21 @@ class GameViewController: UIViewController {
         
         //Present scene
         skView.presentScene(scene)
+        
+        
+        
+    }
+        @objc func buttonAction(_ sender:UIButton!){
+            print("Button tapped")
+        }
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
+
+    func game(){
+        GameInt += 1
+        timeLabel.text = String(GameInt)
     }
     
     override var prefersStatusBarHidden : Bool {
